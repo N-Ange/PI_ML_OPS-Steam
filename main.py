610332b122c1ba247b9363d4b54a9033143e4022
Nadir Angelini <nadir.angelinimail.com>
2023-12-20 20:00:12 -0300

    modificacion ruta api

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ warnings.filterwarnings("ignore")
 
 
 app = FastAPI()
-df_horas_juego = pd.read_parquet("PI_ML_OPS-Steam\data\horas_juego.parquet")
+df_horas_juego = pd.read_parquet("data\horas_juego.parquet")
 #df_reviews_year = pd.read_parquet("PI_ML_OPS-Steam\data\df_reviews_year.parquet")
 #http://127.0.0.1:8000 
 
