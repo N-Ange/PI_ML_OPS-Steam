9fcabaa121f30b17b22882eeebd58110fcfc731c
Nadir Angelini <nadir.angelinimail.com>
2023-12-20 17:33:39 -0300

    carga de ETL

---
+++ functions.py
@@ -0,0 +1,165 @@
+import pandas as pd
+from textblob import TextBlob
+import re
+from dateutil.parser import parse
+
+def tipo_dato(df):
+    ''' 
+    Realiza el analisis de los tipos de dato del dataframe
+
+    La funcion recibe un DataFrame (df) y devuelve un resumen de su contenido incluyendo infomracion sobre los tipos de datos de cada columna.
+
+
+    Parameters:
+    df(pandas.Dataframe):El DataFrame que se busca analizar.
+
+    Returns: 
+        pandas.DataFrame :Un Dataframe con el resumen de cada columna lo cual incluye:
+
+            * nombre_campo: Nombre de columna
+            *tipo_dato: Tipo de dato en cada columna
+            *nulo: Cantidad de valores nuleos en cada columna
+            *no_nulo%: Porcentaje de vaolores no nulos
+            *nulo%: Porcentaje de valores Nulos
+
+    '''
+
+    titulos = {"nombre_campo":[], "tipo_dato":[], "nulo":[], "no_nulo%":[], "nulo%":[]}
+
+    for columna in df.columns:
+        no_nulos_porc = (df[columna].count()/ len(df)) * 100
+        titulos["nombre_campo"].append(columna)
+        titulos["tipo_dato"].append(df[columna].apply(type).unique())
+        titulos["no_nulo%"].append(round(no_nulos_porc,2))
+        titulos["nulo%"].append(round(100-no_nulos_porc,2))
+        titulos["nulo"].append(df[columna].isnull().sum())
+        
+            
+    df_info = pd.DataFrame(titulos)
+
+    return df_info
+
+
+
+def duplicados(df,column):
+    '''
+    Revisa y muestra la fila cuyos valores de la columna se reppiten.
+
+    Recibe un dataframe y el nombre de la columna donde se desee revisar si hay datos duplicados.
+
+    parameters:
+        df(pandas.DataFrame): El DataFrame en el que se buscaran filas duplicadas.
+        coulmn (str): El nombre de la columna donde se buscara los datos duplicados
+
+    returns:
+        pandas.Dataframe or str: Un dataframe con las filas duplicadas. En caso de no haber duplicados devuelve el mensaje "No se encontrar duplicados"
+
+
+    '''
+
+    rows_duplicated = df[df.duplicated(column, False)]
+
+    if rows_duplicated.empty:
+        return "No se encontraron duplicados"
+    
+    return rows_duplicated.sort_values(by=column)
+
+
+
+def obtener_anio(fecha):
+    '''
+        Extrae el año de una fecha con formato Año/Mes/Dia.
+        Si la fecha es nula o no tiene el formato deuvelve "No disponible"
+
+        parameters:
+        fecha (str o float o None): formato Año/Mes/Dia
+
+        return
+            str: El año si esta disponible y es valido, de otra forma devuelve "No disponible" 
+    
+    
+    
+    '''
+    
+
+    if pd.notna(fecha):
+        if re.match(r'^\d{4}-\d{2}-\d{2}$', fecha):
+            return fecha.split('-')[0]
+    return "Dato no disponible"
+
+
+def remplazar_precio (value):
+    '''
+    Remplaza valores de una columna por 0
+
+    La funcion va a tratar de convertir el valor en un numero flotante, si es exitosa se mantiene, si no se deuelve 0
+
+
+parameters:
+    Value: el valor que se intenta convertir.
+
+    Returns: El valor numerico si se pudo convertir o 0 si no.
+
+
+
+    '''
+    if pd.isna(value):
+        return 0
+    try:
+        flotante = float(value)
+        return flotante
+    except:
+        return 0 
+    
+
+def conversion_fecha(fecha):
+    '''
+    Convierte una fecha de un formato especifico a otro
+
+    args:
+        fecha (str): Fecha en formato Monty Day Year
+    
+    return:
+        str:fecha con formato YYYY-MM-DD
+
+    '''
+    match = re.search(r'(\w+\s\d{1,2},\s\d{4})',fecha)
+    if match:
+        fecha_str = match.group(1)
+        try:
+            fecha_dt = pd.to_datetime(fecha_str)
+            return fecha_dt.strftime("%Y-%m-%d")
+        except:
+            return "Fecha inválida"
+    else:
+        return "Formato invalido"
+        
+
+def sentimientos(review):
+    '''
+    Recibe un texto y realiza un analisis de sentimiento devolviendo un valor numerico.
+
+    Se va a usar la libreria TextBlob para analisar los sentimientos del texto.
+
+    Parameters:
+        review (str): El texto que se desea analizar.
+    
+    Returns:
+        int: Un valor numerico que representa el sentimiento
+         
+        -0 Negativo
+        -1 Neutral o sin Review
+        -2 Positivo
+    
+    
+    '''
+
+    if review is None:
+        return 1
+    polarity =TextBlob(review).sentiment.polarity 
+    if polarity < 0:
+        return 0
+    elif polarity > 0:
+        return 2
+    else:
+        return 1
---
+++ main.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+import re
+import functions
+import warnings
+warnings.filterwarnings("ignore")
+
+
+app = FastAPI()
+df_horas_juego = pd.read_parquet("PI_ML_OPS-Steam\data\horas_juego.parquet")
+df_reviews_year = pd.read_parquet("PI_ML_OPS-Steam\data\df_reviews_year.parquet")
+#http://127.0.0.1:8000 
+
+@app.get("/")
+def index():
+    return "hola"
+
+
+
+
+@app.get("/PlayTimeGenre/{genero}")
+def PlayTimeGenre(genero):
+    gener = df_horas_juego[df_horas_juego["genres"].str.lower() ==genero.lower()]
+    if not gener.empty:
+        horas_anio = gener.groupby("release_year")["playtime_forever"].sum().reset_index()
+        max_horas  = horas_anio["playtime_forever"].max()
+        anio = horas_anio.loc[horas_anio["playtime_forever"] == max_horas,"release_year"].iloc[0]
+        return  {anio,"Año de lanzamiento con mas horas jugadas para el Genero:" }
+    
+
+@app.get("/UsersNotRecommend/{anio}")
+def UsersNotRecommend(anio):
+    filtro = (df_reviews_year["reviews_posted"] == anio) & (df_reviews_year["reviews_recommend"] == False) & (df_reviews_year["sentiment_analysis"] == 0)
+    reviews = df_reviews_year[filtro]
+
+    games = reviews.groupby(df_reviews_year["item_id"]).size().reset_index(name="count")
+    games = games.sort_values(by="count", ascending=False)
+
+    resultado2 = [
+        {"Puesto 1": games.iloc[0]["item_id"]},
+        {"Puesto 2": games.iloc[1]["item_id"]},
+        {"Puesto 3": games.iloc[2]["item_id"]}
+    ]
+    return resultado2
+
+      
+   
+    
+'''
+top_por_anio = {} 
+for index, row in resultado.iterrows():
+       anio_info={
+        "item_id": row["item_id"],
+        }
+        resultado = games.head(3)
+
+def UsersNotRecommend(anio):
+
+    filtro = (year["reviews_posted"] == anio) & (year["reviews_recommend"] == False) & (year["sentiment_analysis"] == 0  )
+    reviews = year[filtro] 
+
+    games = reviews.groupby(df_reviews_year["item_id"]).size().reset_index(name="count")
+    games = games.sort_values(by = "count",ascending = False)
+    return games.head(3)
+    '''
