2882132f46a7d9de6ea82440493af262b399071b
Nadir Angelini <nadir.angelinimail.com>
2023-12-26 05:40:26 -0300

    main

--- main.py
+++ main.py
@@ -137,20 +137,41 @@ def UsersRecommend(anio:int):
 
 @app.get("/UsersNotRecommend/{anio}")
 def UsersNotRecommend(anio):
+    
+    '''
+  Devuelve el top 3 de juegos MENOS recomendados por usuarios para el año dado.
+
+  Args:
+    anio (int): Año para el cual se buscan los juegos menos recomendados.
+
+  Returns:
+    dict: Diccionario con el top 3 de juegos menos recomendados, con la estructura {posición: juego}.
+    '''
     try:
-        filtro = (df_data_muestra.query(f"reviews_year == {anio}")  & 
-                  (df_data_muestra["reviews_recommend"] == True) & 
-                  (df_data_muestra["sentiment_analysis"] == 0))
-        reviews = df_data_muestra[filtro]
+        # Filtrar el DataFrame por el año
+        filtered_df = df_data_muestra.query(f"reviews_anio == {anio}")
+        
 
-        games = (reviews.groupby('item_name')['item_name']
-                 .count()
-                 .reset_index(name="count")
-                 .sort_values(by="count", ascending=False)
-                 .head(3))
+        # Filtrar el DataFrame por reseñas no recomendadas
+        filtered_df = filtered_df[filtered_df['reviews_recommend'] == False]
+        
 
-       
-        top_por_anio ={f"Puesto {i+1}":juego for i,juego in enumerate(games["item_name"])}
+        # Filtrar el DataFrame por reseñas con sentimiento negativo
+        filtered_df = filtered_df[filtered_df['sentiment_analysis'] == 0]
+        
+
+        # Obtener el top 3 de juegos menos recomendados
+        not_recommend_counts = filtered_df.groupby('item_name')['item_name'].count().reset_index(name="count").sort_values(by="count", ascending=False).head(3)
+        
+
+        # Convertir el DataFrame a una lista
+        top_3_dict = {f"Puesto {i+1}": juego for i, juego in enumerate(not_recommend_counts['item_name'])}
+        
+        return top_3_dict
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error al obtener los juegos menos recomendados.")
+
         
         return top_por_anio
     except Exception as e:
